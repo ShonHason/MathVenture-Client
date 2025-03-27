eb5b5d63696dbd681db41d9f0897916ce7fd236b
DanAkrabi <danakrabi@gmail.com>
2025-03-27 18:31:58 +0200

    added a - start a lesson functionality with fake data

--- src/context/LessonsContext.tsx
+++ src/context/LessonsContext.tsx
@@ -1,14 +1,38 @@
-import { createContext, Dispatch, SetStateAction } from "react";
+import { createContext } from "react";
 
-export interface LessonsContextType {
-  // onLessons: () => void;
-  // isMenuLessonsActive: boolean;
-  // setIsMenuLessonsActive: React.Dispatch<React.SetStateAction<boolean>>;
-  topics: string[]; // Add topics state
-  setTopics: React.Dispatch<React.SetStateAction<string[]>>; // Add setTopics function
+export interface Question {
+  question: string;
+  options: string[];
+  correctAnswer: string;
 }
 
-// Provide a default value for TypeScript, even if it is temporary
+export interface Topic {
+  subject: string;
+  grade: string;
+  rank: number;
+  questions: Question[];
+}
+
+export interface LessonsContextType {
+  topics: Topic[];
+  setTopics: React.Dispatch<React.SetStateAction<Topic[]>>;
+}
+
 const LessonsContext = createContext<LessonsContextType | null>(null);
 
 export default LessonsContext;
+
+// import { createContext, Dispatch, SetStateAction } from "react";
+
+// export interface LessonsContextType {
+//   // onLessons: () => void;
+//   // isMenuLessonsActive: boolean;
+//   // setIsMenuLessonsActive: React.Dispatch<React.SetStateAction<boolean>>;
+//   topics: string[]; // Add topics state
+//   setTopics: React.Dispatch<React.SetStateAction<string[]>>; // Add setTopics function
+// }
+
+// // Provide a default value for TypeScript, even if it is temporary
+// const LessonsContext = createContext<LessonsContextType | null>(null);
+
+// export default LessonsContext;
