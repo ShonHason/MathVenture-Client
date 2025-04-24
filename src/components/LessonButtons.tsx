d20f7ad1e62268255080698ef2b554e6afba45bc
Roey Biton <roey345@gmail.com>
2025-04-24 14:36:09 +0300

    Fixed the actions buttons inside the lesson to work as they should.

--- src/components/LessonButtons.tsx
+++ src/components/LessonButtons.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './LessonButtons.css';
+import React from "react";
+import "./LessonButtons.css";
 
 interface LessonButtonsProps {
   onActionPerformed?: (action: string) => void;
@@ -7,47 +7,49 @@ interface LessonButtonsProps {
 
 const LessonButtons: React.FC<LessonButtonsProps> = ({ onActionPerformed }) => {
   const handleButtonClick = (action: string) => {
-    // טיפול מקומי בלחיצה על כפתור
     switch (action) {
-      case 'time-out':
-        console.log('הפסקה');
+      case "pause":
+        console.log("הפסקה");
         break;
-      case 'explanation':
-        console.log('הסבר נוסף');
+      case "explanation":
+        console.log("הסבר נוסף");
         break;
-      case 'slow':
-        console.log('הסבר איטי יותר');
+      case "slow":
+        console.log("הסבר איטי יותר");
         break;
-      case 'end-lesson':
-        console.log('סיימת את השיעור');
+      case "end-lesson":
+        console.log("סיימת את השיעור");
         break;
       default:
-        console.log('פעולה לא מוכרת');
+        console.log("פעולה לא מוכרת");
     }
-    
-    // העברת הפעולה לקומפוננטת האב אם נדרש
+
     if (onActionPerformed) {
       onActionPerformed(action);
     }
   };
 
-  const openScanner = () => {
-    // כאן יוכל להיות הקוד שפותח את אפשרות העלאת תמונה או סריקה עם המצלמה
-    console.log('הסריקה החלה!');
-  };
-
   return (
     <div className="button-container">
-      <button className="button" onClick={() => handleButtonClick('time-out')}>
+      <button className="button" onClick={() => handleButtonClick("pause")}>
         הפסקה
       </button>
-      <button className="button" onClick={() => handleButtonClick('explanation')}>
+      <button
+        className="button"
+        onClick={() => handleButtonClick("explanation")}
+      >
         הסבר נוסף
       </button>
-      <button className="button black-button" onClick={() => handleButtonClick('slow')}>
+      <button
+        className="button black-button"
+        onClick={() => handleButtonClick("slow")}
+      >
         לאט יותר
       </button>
-      <button className="button" onClick={() => handleButtonClick('end-lesson')}>
+      <button
+        className="button"
+        onClick={() => handleButtonClick("end-lesson")}
+      >
         סיים שיעור
       </button>
     </div>
