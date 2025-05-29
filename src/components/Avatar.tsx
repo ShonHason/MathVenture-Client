12e72a751240a249ceb20b379056464e08059c99
DanAkrabi <danakrabi@gmail.com>
2025-05-29 15:00:09 +0300

    added speaking indicator

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,12 +3,11 @@ import React from "react";
 export default function Avatar() {
   return (
     <div className="relative mb-6 w-48 h-48 flex items-center justify-center">
-      
-        {/*BUBBLE ANIMATIONS-->
+      {/*BUBBLE ANIMATIONS-->
       <div className="absolute -top-4 -left-4 w-6 h-6 bg-yellow-400 rounded-full animate-bounce"></div>
       <div className="absolute -top-6 -right-4 w-8 h-8 bg-yellow-400 rounded-full animate-bounce delay-150"></div>
         */}
-      
+
       <img
         src="/MathVentureBot.svg"
         alt="Math Venture Bot"
