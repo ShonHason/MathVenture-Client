98d4c05d7922dd00f0c2b3050cd3163263710e94
ShonHason <ShonHason4@gmail.com>
2025-06-25 17:29:09 +0300

    small

--- src/components/SecuritySettings.tsx
+++ src/components/SecuritySettings.tsx
@@ -1,48 +1,4 @@
-// import React from "react";
-// import "./SecuritySettings.css";
-// import { useUser } from "../context/UserContext"; // ודאי שהנתיב נכון
-
-// const SecuritySettings = () => {
-//   const { user } = useUser();
-
-//   return (
-//     <div className="security-settings-container">
-//       <div className="security-setting">
-//         <span>{user?.email || "לא ידוע"}</span>
-//         <p>:אימייל</p>
-//       </div>
-//       <div className="security-setting">
-//         <button>
-//           <span>שינוי סיסמא</span>
-//         </button>
-//         <p>:סיסמה</p>
-//       </div>
-//       <div className="security-setting">
-//         <span>{user?.parent_phone || "לא ידוע"}</span>
-//         <p>:מספר טלפון</p>
-//       </div>
-//       <div className="security-setting">
-//         <span>{user?.opportunities || "לא ידוע"}</span>
-//         <p>:מספר הזדמנויות</p>
-//       </div>
-//       <div className="security-setting security-setting-auth">
-//         <label className="toggle-container">
-//           <input
-//             type="checkbox"
-//             checked={user?.twoFactorAuth || false}
-//             readOnly
-//           />
-//         </label>
-//         <p>:אימות דו שלבי</p>
-//       </div>
-//       <div className="security-setting">
-//         <button className="security-setting-codes">Generate new codes</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SecuritySettings;
+;
 import React from "react";
 import "./SecuritySettings.css";
 import { useUser } from "../context/UserContext"; // ודאי שהנתיב נכון
